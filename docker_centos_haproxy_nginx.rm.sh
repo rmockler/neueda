7853671b4cfe14c928aefda3691df3df4c2b85ec
Your Name <you@example.com>
2020-07-09 20:23:39 +0000

    added explanations at the end of the script.

--- docker_centos_haproxy_nginx.rm.sh
+++ docker_centos_haproxy_nginx.rm.sh
@@ -50,7 +50,8 @@ docker ps -aq|sed 's@.*@docker exec -it \0 /bin/bash -c "yum install -y nginx"@'
 #
 #
 #### 3. configured HAProxy to send traffic to working Nginx servers
-printf 'defaults\nmode http\noption redispatch\nfrontend http_front\nbind *:80\ndefault_backend http_back\n\nlisten stats\nbind *:89\nstats uri /\nstats realm Haproxy\ Statistics\n\nbackend http_back\nbalance roundrobin\nserver nginx.172.17.0.2:81 172.17.0.2:81 check\nserver haproxy.172.17.0.2:89 172.17.0.2:89 check\nserver nginx.172.17.0.3:81 172.17.0.3:81 check\nserver haproxy.172.17.0.3:89 172.17.0.3:89 check\n' |tee ha.cfg
+docker inspect --format='docker exec -it {{.Config.Hostname}} /bin/bash -c "mv /usr/share/nginx/html/index.html /usr/share/nginx/html/index.`date +%d%h%y%H%00`html;echo \<h1\>Nginx {{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}\</h1\> >> /usr/share/nginx/html/index.html"' $(docker ps -aq)|tee exec1 ; source exec1
+
 docker ps -aq|sed 's@.*@docker cp ha.cfg \0:/@'|tee exec1;source exec1
 docker ps -aq|sed 's@.*@docker exec -it \0 /bin/bash -c "haproxy -f ha.cfg -D"@'|tee exec1;source exec1
 docker inspect --format='docker exec -it {{.Config.Hostname}} /bin/bash -c "rm /usr/share/nginx/html/index.html;echo \<h1\>{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}\</h1\> >> /usr/share/nginx/html/index.html"' $(docker ps -aq)|tee exec1 ; source exec1
@@ -59,4 +60,20 @@ docker ps -aq|sed 's@.*@docker exec -it \0 /bin/bash -c "sed '\''s^80 default^81
 #
 #
 #### Testing / Notes ####
+read -t10 -p "Opening live demo or ctrl-c to escape."
+firefox http://52.214.204.249:82/ http://52.214.204.249:82/ https://github.com/rmockler/neueda
+
+printf "\t\t   Testing ans assumptions   \n\n"  
+printf "\t\t   Known issues:    \n"  
+printf "\t\t   HAProxy has warnings as the defaults are missing.\n"  
+printf "\t\t   epel-release not installing for i686 cluster. bug apparantly.\n"  
+printf "\t\t   Centos7 systemd components are not all prenent in this image.\n"  
+printf "\t\t   \n"  
+printf "\t\t   Further impovements:    \n"  
+printf "\t\t   Script as is, is fully functional, but warning coulbe be removed by adding the defaults.\n"  
+printf "\t\t   It is unlikely that this will be run on anything except x86_64, but if required, a case statement could manually install epel by rpm.\n"  
+printf "\t\t   There is a  a centos:systemd image but out-of-scope.\n"  
+
+
+
 
